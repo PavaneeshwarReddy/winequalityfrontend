b2885ee345f4c9539fad41f09f8c63b4a79c970f
PavaneeshwarReddy <pavaneeshwar7077@gmail.com>
2023-03-05 18:42:53 +0530

    aa

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import heatmap from "./images/heatmap.png";
 import Wine from "./class";
-
+import loadingImg from "./images/loading.gif";
 function App() {
   const [fixed_acidity, setFixedAcidity] = useState(0);
   const [volatile_acidity, setVolatileAcidity] = useState(0);
@@ -15,8 +15,10 @@ function App() {
   const [sulphates, setSulphates] = useState(0);
   const [alocholo, setAlochol] = useState(0);
   const [ans, setAns] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const sendData = () => {
+    setLoading(true);
     var wineData = new Wine(
       fixed_acidity,
       volatile_acidity,
@@ -37,33 +39,51 @@ function App() {
       body: JSON.stringify(wineData),
     })
       .then((response) => response.json())
-      .then((data) => setAns(data["Data"]));
-      // fetch("https://winequalitybackendpavaneeshwar.onrender.com/get-by-name"  
-      // )
-      //   .then((response) => response.json())
-      //   .then((data) => setAns(data["name"]));
+      .then((data) => {
+        setAns(data["Data"]);
+        setLoading(false);
+      });
+    // fetch("https://winequalitybackendpavaneeshwar.onrender.com/get-by-name"
+    // )
+    //   .then((response) => response.json())
+    //   .then((data) => setAns(data["name"]));
   };
 
-  const downloadSrcCode=()=>{
+  const downloadSrcCode = () => {
     const url = "https://pavaneeshwar7077-winequality.netlify.app/srccode.pdf";
     const aTag = document.createElement("a");
     aTag.href = url;
-    aTag.setAttribute("download","srccode.pdf")
+    aTag.setAttribute("download", "srccode.pdf");
     document.body.appendChild(aTag);
     aTag.click();
     aTag.remove();
-
-  }
+  };
   return (
-    <div className="flex flex-col w-full  h-full items-center justify-center font-mono">
-      <div className="self-start p-2 font-bold text-red-600">Wine Quality Prediction</div>
-      <button className="bg-green-400 p-3 rounded-full " onClick={()=>downloadSrcCode()}>Download Source Code</button>
-      <div className="flex flex-col text-center mt-28 bg-slate-100 rounded-3xl p-10 space-y-3  ">
-      {ans != null ? (
-        <div className="text-4xl font-bold text-slate-600 mb-4">{ans}</div>
+   <div className="bg-slate-100">
+     {loading ? (
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
+          <img src={loadingImg} alt="loading image..." />
+        </div>
       ) : (
         <></>
       )}
+     <div className={`flex flex-col w-full  h-full items-center justify-center font-mono ${loading==true?"opacity-40":"opacity-100"} `}>
+    
+      <div className={` p-2 text-3xl font-bold text-red-600`}>
+        Wine Quality Prediction
+      </div>
+      <button
+        className="bg-green-400 p-3 rounded-full "
+        onClick={() => downloadSrcCode()}
+      >
+        Download Source Code
+      </button>
+      <div className="flex flex-col text-center mt-28 bg-slate-200 rounded-3xl p-10 space-y-3  ">
+        {ans != null ? (
+          <div className="text-4xl font-bold text-slate-600 mb-4">{ans}</div>
+        ) : (
+          <></>
+        )}
         <div className="text-black text-2xl font-bold">Enter the Values</div>
         <div className="flex flex-row space-x-2">
           <input
@@ -166,11 +186,12 @@ function App() {
           check quality
         </button>
       </div>
-     <div>
-     <div className="font-bold text-center mt-10 text-xl">Heat Map</div>
-     <img src={heatmap} alt="loading..."/>
-     </div>
+      <div>
+        <div className="font-bold text-center mt-10 text-xl">Heat Map</div>
+        <img src={heatmap} alt="loading..." />
+      </div>
     </div>
+   </div>
   );
 }
 
